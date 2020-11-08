b41167e71e03eb6614d091d7be93d3f4228ffc5c
Daiane Silva <daiane.mascarenhas@invillia.com>
2020-11-08 18:13:32 -0300

    chore: useMovieFetch to fetch movie data

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Movie = () => <div>Movie</div>;
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../helpers/config';
+
+// Components
+import Grid from '../components/Grid';
+import Spinner from '../components/Spinner';
+
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+// Image
+import NoImage from '../assets/images/no_image.jpg';
+
+const Movie = () => {
+	const { movieId } = useParams();
+	const { state: movie, loadingo, error } = useMovieFetch(movieId);
+
+	return (
+		<>
+			<div>Movie</div>
+		</>
+	);
+};
 
 export default Movie;
